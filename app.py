54d8f12e35bef77294802942bf76748b959b62aa
Dan <dlea@eou.ed\u>
2021-05-19 16:40:31 -0700

    just getting started

--- app.py
+++ app.py
@@ -8,6 +8,10 @@ app = Flask(__name__)
 def home():
     return render_template('login.html')
 
+@app.route('/create_account')
+def create_account():
+    return render_template('create_account.html')
+
 '''
 @app.route('/')
 def hello_world():
