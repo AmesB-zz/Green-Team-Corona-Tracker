f54a6a45dbaec18cb8f41913caf1076ec9f4b302
Dan <dlea@eou.ed\u>
2021-05-19 15:30:47 -0700

    just getting started and testing system

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
+from flask import (
+    Flask, render_template, redirect, url_for, request
+)
 
-from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
+def home():
+    return render_template('login.html')
+
+'''
+@app.route('/')
 def hello_world():
-    return 'Eastern Oregon University Coronavirus Tracker'
-
-
+    return 'Hello, World!'
+'''
