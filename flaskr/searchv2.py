75e44ecd0032075f3b325653baa98c96554c7d80
Vitor <vitorpedrosa675@gmail.com>
2021-06-05 18:07:37 -0700

    I added a new test data with 100 users. I will add the other 900 after I work on spreding each node out.

--- flaskr/searchv2.py
+++ flaskr/searchv2.py
@@ -3,17 +3,19 @@ import numpy
 import pandas as pd
 import matplotlib.pyplot as plt
 import networkx as nx
-from matplotlib.widgets import TextBox
 ###############
-def getPercetage(thisUser):
+#from .db import get_db
+
+def getReport(thisUser):
     G = nx.Graph()
+    #con = get_db()
     con = sql.connect("../instance/flaskr.sqlite")
-    main = pd.read_sql_query("SELECT name, u.firstName, L.rate from UserLocation join Users U on UserLocation.username = U.username join Location L on UserLocation.location_id = L.location_id where entryTime >= (select entryTime from UserLocation where username in (select username from Users where isInfected = TRUE));", con)
-    infectedUser = pd.read_sql_query( "select firstName from Users where isInfected = TRUE", con)
+    main = pd.read_sql_query("SELECT name, u.username, L.rate from UserLocation join Users U on UserLocation.username = U.username join Location L on UserLocation.location_id = L.location_id where entryTime >= (select entryTime from UserLocation where username in (select username from Users where isInfected = TRUE));", con)
+    infectedUser = pd.read_sql_query( "select username from Users where isInfected = TRUE", con)
     locations = pd.read_sql_query("SELECT name, rate from location", con);
     infectedUserFlatten = infectedUser.values.flatten()
     conn = sql.connect(':memory:')
-    G = nx.from_pandas_edgelist(main, 'firstName', 'name', edge_attr='rate')
+    G = nx.from_pandas_edgelist(main, 'username', 'name', edge_attr='rate')
     path = nx.shortest_path(G, source=infectedUserFlatten[0], target=thisUser)
     shortestPath = {"Locations": path}
     pathFrame = pd.DataFrame(data=shortestPath)
@@ -24,13 +26,16 @@ def getPercetage(thisUser):
     realRate = result['rate']
     arr = realRate.values.flatten()
     percentage = numpy.prod(arr)
-    #nx.draw(G, with_labels=True, font_weight='bold')
     pos = nx.spring_layout(G)
-    options = {"node_size": 500, "alpha": 0.8}
-    nx.draw(G, pos, with_labels=True, node_color='#89CFF0',node_shape="p")
+    #nx.draw(G,pos, with_labels=True, font_weight='bold')
+
+    options = {"node_size": 500, "alpha": 0.9}
+    #nx.draw(G, pos, font_size=6, with_labels=True, node_color='#89CFF0',node_shape="p", font_weight='bold')
     path_edges = list(zip(path, path[1:]))
-    nx.draw_networkx_nodes(G, pos, nodelist=path, node_color='#fa4a37', **options)
-    nx.draw_networkx_edges(G, pos, edgelist=path_edges, edge_color='#fa4a37', width=10, **options)
+    labels = {k: k for k in path}
+    nx.draw_networkx_nodes(G, pos, nodelist=path, node_color='#89CFF0',node_shape="p", **options)
+    nx.draw_networkx_edges(G, pos, edgelist=path_edges, edge_color='#89CFF0',node_shape="p", width=10, **options)
+    nx.draw_networkx_labels(G, pos, labels)
     plt.axis('equal')
     pretty = "Your rate of infection is: {d}%".format(d =percentage * 100)
     plt.text(0.02, 0.02, pretty, fontsize=14, transform=plt.gcf().transFigure)
@@ -38,7 +43,7 @@ def getPercetage(thisUser):
     plt.show()
 
 def changeInfectedUser(thisUser):
-    con = sql.connect("../instance/flaskr.sqlite")
+    con = get_db()
     cur = con.cursor()
     cur.execute("update users set isInfected = 0 where 1=1")
     qry = "update users set isInfected = true where ? = username"
@@ -46,12 +51,29 @@ def changeInfectedUser(thisUser):
     con.commit()
 
 def addLocation(name, rate):
-    con = sql.connect("../instance/flaskr.sqlite")
+    con = get_db()
     cur = con.cursor()
     qry = "insert into location (name, rate) values(?, ?)"
     cur.execute(qry, (name,rate))
     con.commit()
-getPercetage("Lilas")
+
+def populate():
+    con = sql.connect("../instance/flaskr.sqlite")
+    cur = con.cursor()
+    userLocations = open("../DB/populateUserLocation")
+    locations = open("../DB/populateLocation")
+    users = open("../DB/populateUsers")
+    userLocationsSql = userLocations.read()
+    locationsSql = locations.read()
+    usersSql = users.read()
+    cur.executescript(userLocationsSql)
+    cur.executescript(usersSql)
+    cur.executescript(locationsSql)
+    cur.execute("update UserLocation set rate = (select rate from Location where location_id = UserLocation.location_id) where exists (select rate from Location where location_id = UserLocation.location_id);")
+    cur.execute("update UserLocation set username = (select u.username from Users u where UserLocation.username = u.ROWID);")
+    con.commit()
+
+
 
 
 
