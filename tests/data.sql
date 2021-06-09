264a251cb6869169abc3371b3008334ec38749d7
Bryan Ames <amesb@eou.edu>
2021-06-09 15:48:31 -0700

    first go at infection rate test

--- tests/data.sql
+++ tests/data.sql
@@ -2,7 +2,7 @@
 INSERT INTO Users (username, PasswordHash, firstName, lastName, isInfected, isAdmin)
 VALUES
     ('test', 'pbkdf2:sha256:50000$TCI4GzcX$0de171a4f4dac32e3364c7ddc7c14f3e2fa61f2d17574483f7ffbb431b4acb2f', 'Test', 'User', False, True),
-    ('other', 'pbkdf2:sha256:50000$kJPKsz6N$d2d4784f1b030a9761f5ccaeeaca413f27f2ecb76d6168407af962ddce849f79', 'Other', 'User', False, False),
+    ('other', 'pbkdf2:sha256:50000$kJPKsz6N$d2d4784f1b030a9761f5ccaeeaca413f27f2ecb76d6168407af962ddce849f79', 'Other', 'User', False, False);
 
 -- create locations
 insert into Location (location_id, name, rate)  values (1,'Safeway',.06);
--- tests/test_test_user_index.py
+++ tests/test_test_user_index.py
@@ -1,3 +1,8 @@
 import pytest
 from flask import g, session
 from flaskr.db import get_db
+from flaskr.test_user_index import getReport
+
+def test_get_report(app):
+	with app.app_context():
+		assert getReport("eringwood0") == 0
